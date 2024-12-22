2f6b63391de5b93fc08b9263c99d3f36656596ab
hillcrestbank <johnberntsen59@gmail.com>
2024-12-22 10:29:20 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -20,41 +20,46 @@ $("#obankmodal").modal("attach events", "#obankbtnweb", "show");
 $("#obankmodal").modal("attach events", "#obankbtnmob", "show");
 $("#obankmodal").modal("attach events", "#obankbtntab", "show");
 
-document.getElementById("loginButton").addEventListener("click", function () {
-  var username = document.getElementById("uname").value;
-  var password = document.getElementById("pword").value;
+if(document.getElementById('loginButton')){
+  document.getElementById("loginButton").addEventListener("click", function () {
+    var username = document.getElementById("uname").value;
+    var password = document.getElementById("pword").value;
 
-  if (username === "patrmac55" && password === "FireRocket78#") {
-    document.getElementById("loader").style.display = "block";
-    setTimeout(function () {
-      window.location.href = "dashboard.html";
-    }, 5000);
-  } else {
-    $(".ui.form")
-      .form({
-        fields: {
-          uname: {
-            identifier: "uname",
-            rules: [
-              {
-                type: "empty",
-                prompt: "Please enter your username",
-              },
-            ],
+    if (username === "patrmac55" && password === "FireRocket78#") {
+      document.getElementById("loader").style.display = "block";
+      setTimeout(function () {
+        window.location.href = "dashboard.html";
+      }, 5000);
+    } else {
+      $(".ui.form")
+        .form({
+          fields: {
+            uname: {
+              identifier: "uname",
+              rules: [
+                {
+                  type: "empty",
+                  prompt: "Please enter your username",
+                },
+              ],
+            },
+            pword: {
+              identifier: "pword",
+              rules: [
+                {
+                  type: "empty",
+                  prompt: "Please enter your password",
+                },
+              ],
+            },
           },
-          pword: {
-            identifier: "pword",
-            rules: [
-              {
-                type: "empty",
-                prompt: "Please enter your password",
-              },
-            ],
-          },
-        },
-      })
-      .form("add errors", [
-        "The username or password is invalid. Please try again or contact your branch adminstrator for support.",
-      ]);
-  }
-});
+        })
+        .form("add errors", [
+          "The username or password is invalid. Please try again or contact your branch adminstrator for support.",
+        ]);
+    }
+  });
+}
+
+
+
